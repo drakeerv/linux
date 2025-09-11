planet-community/linux-planet-mainline

HACK: drm/msm: Run mdp5 encoder enable()/disable() after bridge hooks
For some reason the mdp5 encoder must be started after the DSI video
stream is active, otherwise there is just an endless spam of

	[   11.686336] dsi_err_worker: 4440 callbacks suppressed
	[   11.686351] dsi_err_worker: status=5
	[   11.686382] dsi_err_worker: status=4
	[   11.686407] dsi_err_worker: status=4
	[   11.686427] dsi_err_worker: status=4
	...

There does not seem to be a good encoder hook to use this at the
moment, so introduce some temporary hacks to delay the existing
callback.

Signed-off-by: Stephan Gerhold <stephan@gerhold.net>

drivers/gpu/drm/drm_atomic_helper.c
@@ -1165,6 +1165,9 @@ disable_outputs(struct drm_device *dev, struct drm_atomic_state *old_state)
 
 		/* Right function depends upon target state. */
 		if (funcs) {
+			if (funcs->late_enable)
+				drm_atomic_bridge_chain_post_disable(bridge, old_state);
+
 			if (funcs->atomic_disable)
 				funcs->atomic_disable(encoder, old_state);
 			else if (new_conn_state->crtc && funcs->prepare)
@@ -1175,7 +1178,8 @@ disable_outputs(struct drm_device *dev, struct drm_atomic_state *old_state)
 				funcs->dpms(encoder, DRM_MODE_DPMS_OFF);
 		}
 
-		drm_atomic_bridge_chain_post_disable(bridge, old_state);
+		if (!funcs || !funcs->late_enable)
+			drm_atomic_bridge_chain_post_disable(bridge, old_state);
 	}
 
 	for_each_oldnew_crtc_in_state(old_state, crtc, old_crtc_state, new_crtc_state, i) {
@@ -1505,6 +1509,9 @@ void drm_atomic_helper_commit_modeset_enables(struct drm_device *dev,
 		drm_atomic_bridge_chain_pre_enable(bridge, old_state);
 
 		if (funcs) {
+			if (funcs->late_enable)
+				drm_atomic_bridge_chain_enable(bridge, old_state);
+
 			if (funcs->atomic_enable)
 				funcs->atomic_enable(encoder, old_state);
 			else if (funcs->enable)
@@ -1513,7 +1520,8 @@ void drm_atomic_helper_commit_modeset_enables(struct drm_device *dev,
 				funcs->commit(encoder);
 		}
 
-		drm_atomic_bridge_chain_enable(bridge, old_state);
+		if (!funcs || !funcs->late_enable)
+			drm_atomic_bridge_chain_enable(bridge, old_state);
 	}
 
 	drm_atomic_helper_commit_writebacks(dev, old_state);

drakeerv/linux

drivers/gpu/drm/drm_atomic_helper.c
@@ -1211,10 +1211,12 @@ encoder_bridge_disable(struct drm_device *dev, struct drm_atomic_state *state)
 		 * it away), so we won't call disable hooks twice.
 		 */
 		bridge = drm_bridge_chain_get_first_bridge(encoder);
-		drm_atomic_bridge_chain_disable(bridge, state);
 
 		/* Right function depends upon target state. */
 		if (funcs) {
+			if (funcs->late_enable)
+				drm_atomic_bridge_chain_disable(bridge, state);
+
 			if (funcs->atomic_disable)
 				funcs->atomic_disable(encoder, state);
 			else if (new_conn_state->crtc && funcs->prepare)
@@ -1224,6 +1226,9 @@ encoder_bridge_disable(struct drm_device *dev, struct drm_atomic_state *state)
 			else if (funcs->dpms)
 				funcs->dpms(encoder, DRM_MODE_DPMS_OFF);
 		}
+
+		if (!funcs || !funcs->late_enable)
+			drm_atomic_bridge_chain_disable(bridge, state);
 	}
 }
 
@@ -1646,6 +1651,9 @@ encoder_bridge_enable(struct drm_device *dev, struct drm_atomic_state *state)
 		bridge = drm_bridge_chain_get_first_bridge(encoder);
 
 		if (funcs) {
+			if (funcs->late_enable)
+				drm_atomic_bridge_chain_enable(bridge, state);
+
 			if (funcs->atomic_enable)
 				funcs->atomic_enable(encoder, state);
 			else if (funcs->enable)
@@ -1654,7 +1662,8 @@ encoder_bridge_enable(struct drm_device *dev, struct drm_atomic_state *state)
 				funcs->commit(encoder);
 		}
 
-		drm_atomic_bridge_chain_enable(bridge, state);
+		if (!funcs || !funcs->late_enable)
+			drm_atomic_bridge_chain_enable(bridge, state);
 	}
 }
 